eca73e7ea1e2528414c03bd9b3d4d3a976709249
juniorpen01 <01juniorpen@gmail.com>
2026-01-05 03:56:19 +0800

    Bring in SFML

--- snake-game/main.cpp
+++ snake-game/main.cpp
@@ -1,5 +1,33 @@
+/*!
+* i cant get the warning crap to work for exclusively my code rahhhh
+*/
+
 #include<print>
+
+#include<SFML/Graphics.hpp>
+
+#include "snake.h"
+
+void run();
 
 int main() {
 	std::println("hello world");
+
+	Snake snake{ -1 };
+
+	run();
+}
+
+void run() {
+	sf::RenderWindow window{ sf::VideoMode{{800,600}}, "Snake" };
+	while (window.isOpen()) {
+		while (std::optional<sf::Event> event{ window.pollEvent() }) {
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		window.clear(sf::Color::Magenta);
+
+		window.display();
+	}
 }
---
+++ snake-game/snake.h
@@ -0,0 +1,8 @@
+#pragma once
+class Snake {
+private:
+	int tmp;
+public:
+	Snake(int tmp);
+};
+
